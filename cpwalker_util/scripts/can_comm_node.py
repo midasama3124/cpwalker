e038e8baa3ece31722de21644925834198171f5c
Daniel G <dagv21@live.com>
2019-11-28 03:23:08 -0500

    CAN communication between Rpi and sensor acquisition board achieved. Waiting for proper reorganization of utilities and ROS nodes.

--- cpwalker_util/scripts/can_comm_node.py
+++ cpwalker_util/scripts/can_comm_node.py
@@ -15,33 +15,30 @@ class CANbus(object):
         Pack data: 0
     """
     def send_init(self):
-        id = 0x68  
+        id = 68  
 	# for i in range(10):      
 	msg = can.Message(arbitration_id=id, extended_id=False, data=[0, 0, 0, 0, 0, 0, 0, 0])   # 11-bit identifier
-	print msg
-	# msg = can.Message(arbitration_id=0xc0ffee, data=[id, 0], extended_id=False)
+	#print msg
 	rospy.logwarn("Sending initialization message (ID: {})!".format(id))
 	self.bus.send(msg)
-        # Issue #3: Need to keep running to ensure the writing threads stay alive. ?
-        #time.sleep(1)
 
     def receive_data(self):
-        message = self.bus.recv(1) # Timeout in seconds.
+        message = self.bus.recv() # Timeout in seconds.
         if message is None:
                 rospy.logerr('Timeout occurred, no message.')
 	else:
-        	rospy.loginfo(message)
+		rospy.loginfo(message)
 
 def main():
 	# module = CANbus(80, 'left_hip')
 	# module = CANbus(85, 'right_hip')
 	# module = CANbus(90, 'left_knee')
 	module = CANbus(95, 'right_knee')
-	r = rospy.Rate(100)      # Hz
+	r = rospy.Rate(50)      # Hz
 	while not rospy.is_shutdown():
 		module.send_init()
-		# module.receive_data()
 		r.sleep()
+		#module.receive_data()
 
 if __name__ == "__main__":
     try:
